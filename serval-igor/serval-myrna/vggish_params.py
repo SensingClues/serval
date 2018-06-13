215479bbe8e760e752143e96b166c617f14a11ca
Myrna van de Burgwal <myrna.van.de.burgwal@dikw.com>
2018-06-13 15:46:18 +0200

    Show_label_combinations en Wavfile_to_tfrecord aangemaakt

--- serval-igor/serval-myrna/vggish_params.py
+++ serval-igor/serval-myrna/vggish_params.py
@@ -45,14 +45,8 @@ LEARNING_RATE = 1e-4  # Learning rate for the Adam optimizer.
 ADAM_EPSILON = 1e-8  # Epsilon for the Adam optimizer.
 
 # Names of ops, tensors, and features.
-VGGISH_MODEL = 'models/vggish_model.ckpt'
-VGGISH_PCA_PARAMS = 'models/vggish_pca_params.npz'
 VGGISH_INPUT_TENSOR_NAME = 'vggish/input_features:0'
 VGGISH_OUTPUT_TENSOR_NAME = 'vggish/embedding:0'
-
-YOUTUBE_CHECKPOINT_FILE = 'models/youtube_model.ckpt'
-
-CLASS_LABELS_INDICES = 'models/class_labels_indices.csv'
 
 # Predictions filter
 PREDICTIONS_COUNT_LIMIT = 20
