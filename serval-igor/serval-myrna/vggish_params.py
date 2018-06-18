8095ce270a3a9d20d0fdd56287896482b4b17805
Myrna van de Burgwal <myrna.van.de.burgwal@dikw.com>
2018-06-18 13:28:59 +0200

    Label_occurences netter gemaakt

--- serval-igor/serval-myrna/vggish_params.py
+++ serval-igor/serval-myrna/vggish_params.py
@@ -45,10 +45,13 @@ LEARNING_RATE = 1e-4  # Learning rate for the Adam optimizer.
 ADAM_EPSILON = 1e-8  # Epsilon for the Adam optimizer.
 
 # Names of ops, tensors, and features.
-VGGISH_MODEL = 'models/vggish_model.ckpt'
-VGGISH_PCA_PARAMS = 'models/vggish_pca_params.npz'
+VGGISH_MODEL = 'yt8m/vggish_model.ckpt'
+VGGISH_PCA_PARAMS = 'yt8m/vggish_pca_params.npz'
 VGGISH_INPUT_TENSOR_NAME = 'vggish/input_features:0'
 VGGISH_OUTPUT_TENSOR_NAME = 'vggish/embedding:0'
+
+YOUTUBE_CHECKPOINT_FILE = 'models/model.ckpt-10'
+CLASS_LABELS_INDICES = 'csv_files/class_labels_indices_amsterdam.csv'
 
 # Predictions filter
 PREDICTIONS_COUNT_LIMIT = 20
